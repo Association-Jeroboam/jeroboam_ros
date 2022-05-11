436c3d9f12a8e4ef2ba6a87bec5f9af48a589692
Thomas Jeantet <tj.thomas.jeantet@gmail.com>
2022-05-11 02:39:05 -0700

    threads

--- jrb_can_bridge/src/jrb_can_bridge.cpp
+++ jrb_can_bridge/src/jrb_can_bridge.cpp
@@ -37,7 +37,8 @@ int send_can_frame(struct can_frame * frame);
 void * canardSpecificAlloc(CanardInstance * instance, size_t amount);
 void canardSpecificFree(CanardInstance * instance, void * pointer);
 void initCanard(void);
-void checkTxQueue(void);
+void* checkTxQueue(void*);
+void* checkRxMsg(void*);
 bool pushQueue(const CanardTransferMetadata* const metadata,
                const size_t                        payload_size,
                const void* const                   payload);
@@ -49,13 +50,16 @@ void print_usage(void) {
 
 int canIFace;
 
-pthread_mutex_t execution_lock;
 pthread_mutex_t queue_lock;
+pthread_cond_t  tx_exec_cond = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t tx_exec_lock;
 
 static CanardInstance instance;
 CanardTxQueue  queue;
 CanardRxSubscription subscriptions[CAN_RX_MAX_SUBSCRIPTION];
 unsigned int subCnt;
+
+pthread_t txThread, rxThread;
 
 class MinimalPublisher : public rclcpp::Node
 {
@@ -106,7 +110,7 @@ int main(int argc, char * argv[])
   subscribe(CanardTransferKindMessage, portID, reg_udral_physics_kinematics_cartesian_State_0_1_EXTENT_BYTES_);
   subscribe(CanardTransferKindMessage, uavcan_node_Heartbeat_1_0_FIXED_PORT_ID_, uavcan_node_Heartbeat_1_0_EXTENT_BYTES_);
 
-  if (pthread_mutex_init(&execution_lock, NULL) != 0)
+  if (pthread_mutex_init(&tx_exec_lock, NULL) != 0)
   {
       printf("\nexecution  mutex init failed\n");
       return 1;
@@ -118,38 +122,117 @@ int main(int argc, char * argv[])
       return 1;
   }
 
+  pthread_create(&txThread, NULL, &checkTxQueue, NULL);
+  pthread_create(&rxThread, NULL, &checkRxMsg, NULL);
+
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<MinimalPublisher>());
   rclcpp::shutdown();
   return 0;
 }
 
-void checkTxQueue(void) {
+void* checkTxQueue(void*) {
+
+  while(true) {
+    pthread_mutex_lock(&tx_exec_lock);
+    pthread_cond_wait(&tx_exec_cond, &tx_exec_lock);
+
+
     const CanardTxQueueItem* item = canardTxPeek(&queue);
 
     while(item != NULL) {
-        CanardTxQueueItem* extractedItem = canardTxPop(&queue, item);
-        uint32_t           size          = item->frame.payload_size;
+      CanardTxQueueItem* extractedItem = canardTxPop(&queue, item);
+      uint32_t           size          = item->frame.payload_size;
 
-        do {
-            struct can_frame frame;
-            frame.can_id = item->frame.extended_can_id | 1 << 31;
+      do {
+        struct can_frame frame;
+        frame.can_id = item->frame.extended_can_id | 1 << 31;
 
-            if (size >= MAX_FRAME_SIZE) {
-                frame.can_dlc = MAX_FRAME_SIZE;
-                size -= MAX_FRAME_SIZE;
-            } else {
-                frame.can_dlc= size;
-                size      = 0;
-            }
-            memcpy(&frame.data, item->frame.payload, frame.can_dlc);
+        if (size >= MAX_FRAME_SIZE) {
+            frame.can_dlc = MAX_FRAME_SIZE;
+            size -= MAX_FRAME_SIZE;
+        } else {
+            frame.can_dlc= size;
+            size      = 0;
+        }
+        memcpy(&frame.data, item->frame.payload, frame.can_dlc);
 
-            send_can_frame(&frame);
-        } while (size > 0);
+        send_can_frame(&frame);
+      } while (size > 0);
 
-        instance.memory_free(&instance, extractedItem);
-        item = canardTxPeek(&queue);
+      instance.memory_free(&instance, extractedItem);
+      item = canardTxPeek(&queue);
     }
+
+    pthread_mutex_unlock(&tx_exec_lock);
+
+  }
+}
+
+void* checkRxMsg(void*) {
+  while (true) {
+    if(canIFace !=0) {
+      struct can_frame rx_frame;
+      //this is blocking
+      int nbytes = read(canIFace, &rx_frame, sizeof(struct can_frame));
+
+      if (nbytes >= 0) {
+        const CanardMicrosecond timestamp = 0;
+        CanardFrame frame;
+        frame.extended_can_id = rx_frame.can_id & CAN_EXT_ID_MASK;
+        frame.payload_size = rx_frame.can_dlc;
+        frame.payload = rx_frame.data;
+
+        CanardRxTransfer transfer;
+        CanardRxSubscription * sub;
+        int32_t ret = canardRxAccept(&instance, timestamp, &frame, 0, &transfer, &sub);
+        if(ret == 1) {
+          //success
+
+          // process message
+          reg_udral_physics_kinematics_cartesian_State_0_1 state;
+          bool state_rcvd = false;
+          if(transfer.metadata.port_id == 0) {
+
+            state_rcvd = true;
+            reg_udral_physics_kinematics_cartesian_State_0_1_deserialize_(&state,
+                                                                          (uint8_t *)transfer.payload,
+                                                                          &transfer.payload_size);
+          }
+          //then
+          instance.memory_free(&instance, transfer.payload);
+
+          if(state_rcvd) {
+            for (int i = 0; i < 3; ++i) {
+              printf("%.4f ", state.pose.position.value.meter[i]);
+            }
+            printf("\n");
+            for (int i = 0; i < 4; ++i) {
+              printf("%.4f ", state.pose.orientation.wxyz[i]);
+            }
+            printf("\n");
+            for (int i = 0; i < 3; ++i) {
+              printf("%.4f ", state.twist.linear.meter_per_second[i]);
+            }
+            printf("\n");
+            for (int i = 0; i < 3; ++i) {
+              printf("%.4f ", state.twist.angular.radian_per_second[i]);
+            }
+            printf("\n");
+
+          }
+
+        } else if (ret == 0) {
+            // rejected because not subscribed or transfer not complete
+        }else {
+            //error
+            printf("frame error %i\n", ret);
+        }
+      }
+    } else {
+      usleep(10);
+    }
+  }
 }
 
 bool pushQueue(const CanardTransferMetadata* const metadata,
@@ -159,6 +242,10 @@ bool pushQueue(const CanardTransferMetadata* const metadata,
     pthread_mutex_lock(&queue_lock); // prevents other threads from pushing in the queue at the same time
     int32_t res = canardTxPush(&queue, &instance, 0, metadata, payload_size, payload);
     pthread_mutex_unlock(&queue_lock);
+    pthread_mutex_lock(&tx_exec_lock);
+    pthread_cond_signal(&tx_exec_cond);
+    pthread_mutex_unlock(&tx_exec_lock);
+    
     success = (0 <= res);
     return success;
 }
