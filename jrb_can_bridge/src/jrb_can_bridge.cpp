bc7b6c4afd351ea3652a5f725de17fd33d458ded
Thomas Jeantet <tj.thomas.jeantet@gmail.com>
2022-05-23 06:42:43 -0700

    added motion and pid configs

--- jrb_can_bridge/src/jrb_can_bridge.cpp
+++ jrb_can_bridge/src/jrb_can_bridge.cpp
@@ -18,6 +18,9 @@
 #include "jrb_msgs/msg/pid_state.hpp"
 #include "jrb_msgs/msg/pump_status.hpp"
 #include "jrb_msgs/msg/valve_status.hpp"
+#include "jrb_msgs/msg/pid_config.hpp"
+#include "jrb_msgs/msg/adaptative_pid_config.hpp"
+#include "jrb_msgs/msg/motion_config.hpp"
 #include <net/if.h>
 #include <sys/ioctl.h>
 #include <sys/socket.h>
@@ -32,6 +35,9 @@
 #include "PIDState_0_1.h"
 #include "PumpStatus_0_1.h"
 #include "ValveStatus_0_1.h"
+#include "PIDConfig_0_1.h"
+#include "AdaptativePIDConfig_0_1.h"
+#include "MotionConfig_0_1.h"
 
 using namespace std::chrono_literals;
 
@@ -98,6 +104,12 @@ class CanBridge : public rclcpp::Node
         "left_valve_status", 4, std::bind(&CanBridge::valveLeftCB, this, std::placeholders::_1));
       right_valve_sub = this->create_subscription<jrb_msgs::msg::ValveStatus>(
         "right_valve_status", 4, std::bind(&CanBridge::valveRightCB, this, std::placeholders::_1));
+      left_adpat_pid_conf_sub = this->create_subscription<jrb_msgs::msg::PumpStatus>(
+        "left_adapt_pid_conf", 4, std::bind(&CanBridge::leftAdpatPidConfCB, this, std::placeholders::_1));
+      right_adpat_pid_conf_sub = this->create_subscription<jrb_msgs::msg::PumpStatus>(
+        "left_adapt_pid_conf", 4, std::bind(&CanBridge::adpatPidConfCB, this, std::placeholders::_1));
+      motion_config_sub = this->create_subscription<jrb_msgs::msg::PumpStatus>(
+        "motion_config", 4, std::bind(&CanBridge::motionConfigCB, this, std::placeholders::_1));
     }
 
   
@@ -170,6 +182,21 @@ class CanBridge : public rclcpp::Node
       right_valve_pub->publish(status_msg);
     }
   private:
+    static void send_can_msg(CanardPortID portID, CanardTransferID* transferID, void* buffer, size_t buf_size) {
+       CanardTransferMetadata metadata;
+        metadata.priority = CanardPriorityNominal;
+        metadata.transfer_kind = CanardTransferKindMessage;
+        metadata.port_id = portID;
+        metadata.remote_node_id = CANARD_NODE_ID_UNSET;
+        metadata.transfer_id = *transferID;
+
+
+        bool success = pushQueue(&metadata, buf_size, buffer);
+        if (!success ) {
+            printf("Queue push failed\n");
+        }
+        (*transferID)++;
+    }
     void robot_twist_goal_cb(const geometry_msgs::msg::Twist::SharedPtr msg) const {
         static CanardTransferID transfer_id = 0;
         reg_udral_physics_kinematics_cartesian_Twist_0_1 twist;
@@ -186,19 +213,7 @@ class CanBridge : public rclcpp::Node
 
         reg_udral_physics_kinematics_cartesian_Twist_0_1_serialize_(&twist, buffer, &buf_size);
 
-        CanardTransferMetadata metadata;
-        metadata.priority = CanardPriorityNominal,
-        metadata.transfer_kind = CanardTransferKindMessage,
-        metadata.port_id = ROBOT_TWIST_GOAL_ID,
-        metadata.remote_node_id = CANARD_NODE_ID_UNSET,
-        metadata.transfer_id = transfer_id,
-
-        transfer_id++;
-        bool success = pushQueue(&metadata, buf_size, buffer);
-        if (!success ) {
-            printf("Queue push failed\n");
-        }
-
+        send_can_msg(ROBOT_TWIST_GOAL_ID, &transfer_id, buffer, buf_size);
     }
 
     void pumpLeftCB(const jrb_msgs::msg::PumpStatus::SharedPtr msg) const {
@@ -212,19 +227,8 @@ class CanBridge : public rclcpp::Node
 
         jeroboam_datatypes_actuators_pneumatics_PumpStatus_0_1_serialize_(&pumpStatus, buffer, &buf_size);
 
-        CanardTransferMetadata metadata;
-        metadata.priority = CanardPriorityNominal,
-        metadata.transfer_kind = CanardTransferKindMessage,
-        metadata.port_id = ACTION_PUMP_SET_STATUS_ID,
-        metadata.remote_node_id = CANARD_NODE_ID_UNSET,
-        metadata.transfer_id = transfer_id,
-
-        transfer_id++;
-
-        bool success = pushQueue(&metadata, buf_size, buffer);
-        if (!success ) {
-            printf("Queue push failed\n");
-        }
+        send_can_msg(ACTION_PUMP_SET_STATUS_ID, &transfer_id, buffer, buf_size);
+      
     }
 
     void pumpRightCB(const jrb_msgs::msg::PumpStatus::SharedPtr msg) const {
@@ -238,19 +242,8 @@ class CanBridge : public rclcpp::Node
 
         jeroboam_datatypes_actuators_pneumatics_PumpStatus_0_1_serialize_(&pumpStatus, buffer, &buf_size);
 
-        CanardTransferMetadata metadata;
-        metadata.priority = CanardPriorityNominal,
-        metadata.transfer_kind = CanardTransferKindMessage,
-        metadata.port_id = ACTION_PUMP_SET_STATUS_ID,
-        metadata.remote_node_id = CANARD_NODE_ID_UNSET,
-                metadata.transfer_id = transfer_id,
-
-                transfer_id++;
-
-        bool success = pushQueue(&metadata, buf_size, buffer);
-        if (!success ) {
-            printf("Queue push failed\n");
-        }
+        send_can_msg(ACTION_PUMP_SET_STATUS_ID, &transfer_id, buffer, buf_size);
+      
     }
 
     void valveLeftCB(const jrb_msgs::msg::ValveStatus::SharedPtr msg) const {
@@ -265,18 +258,7 @@ class CanBridge : public rclcpp::Node
 
         jeroboam_datatypes_actuators_pneumatics_ValveStatus_0_1_serialize_(&valveStatus, buffer, &buf_size);
 
-        CanardTransferMetadata metadata;
-        metadata.priority = CanardPriorityNominal,
-        metadata.transfer_kind = CanardTransferKindMessage,
-        metadata.port_id = ACTION_VALVE_SET_STATUS_ID,
-        metadata.remote_node_id = CANARD_NODE_ID_UNSET,
-        metadata.transfer_id = transfer_id,
-        transfer_id++;
-
-        bool success = pushQueue(&metadata, buf_size, buffer);
-        if (!success ) {
-            printf("Queue push failed\n");
-        }
+        send_can_msg(ACTION_VALVE_SET_STATUS_ID, &transfer_id, buffer, buf_size);
     }
 
     void valveRightCB(const jrb_msgs::msg::ValveStatus::SharedPtr msg) const {
@@ -291,19 +273,76 @@ class CanBridge : public rclcpp::Node
 
         jeroboam_datatypes_actuators_pneumatics_ValveStatus_0_1_serialize_(&valveStatus, buffer, &buf_size);
 
-        CanardTransferMetadata metadata;
-        metadata.priority = CanardPriorityNominal,
-        metadata.transfer_kind = CanardTransferKindMessage,
-        metadata.port_id = ACTION_VALVE_SET_STATUS_ID,
-        metadata.remote_node_id = CANARD_NODE_ID_UNSET,
-        metadata.transfer_id = transfer_id,
-        transfer_id++;
-
-        bool success = pushQueue(&metadata, buf_size, buffer);
-        if (!success ) {
-            printf("Queue push failed\n");
-        }
+        send_can_msg(ACTION_VALVE_SET_STATUS_ID, &transfer_id, buffer, buf_size);
     }
+
+
+    void leftAdpatPidConfCB( const jrb_msgs::msg::AdaptativePIDConfig msg) const {
+      static CanardTransferID transfer_id = 0;
+
+      jeroboam_datatypes_actuators_motion_AdaptativePIDConfig_0_1 adaptConfig;
+      adaptConfig.ID = msg.id;
+      for(uint8_t conf_idx = 0; conf_idx < 3; conf_idx++) {
+        for(uint8_t pid_idx = 0; pid_idx < 3; pid_idx++) {
+          adaptConfig.configs[conf_idx].pid[pid_idx] = msg.configs[conf_idx].pid[pid_idx];
+        }
+        adaptConfig.configs[conf_idx].bias = msg.configs[conf_idx].bias;
+        adaptConfig.thresholds[conf_idx] = msg.thresholds[conf_idx];
+      }
+      
+
+      size_t buf_size = jeroboam_datatypes_actuators_motion_AdaptativePIDConfig_0_1_SERIALIZATION_BUFFER_SIZE_BYTES_;
+      uint8_t buffer[jeroboam_datatypes_actuators_motion_AdaptativePIDConfig_0_1_SERIALIZATION_BUFFER_SIZE_BYTES_];
+
+      jeroboam_datatypes_actuators_motion_AdaptativePIDConfig_0_1_serialize_(&adaptConfig, buffer, &buf_size);
+
+      send_can_msg(MOTION_SET_ADAPTATIVE_PID_ID, &transfer_id, buffer, buf_size);
+    }
+    
+    void adpatPidConfCB( const jrb_msgs::msg::AdaptativePIDConfig msg) const {
+      static CanardTransferID transfer_id = 0;
+
+      jeroboam_datatypes_actuators_motion_AdaptativePIDConfig_0_1 adaptConfig;
+      adaptConfig.ID = msg.id;
+      for(uint8_t conf_idx = 0; conf_idx < 3; conf_idx++) {
+        for(uint8_t pid_idx = 0; pid_idx < 3; pid_idx++) {
+          adaptConfig.configs[conf_idx].pid[pid_idx] = msg.configs[conf_idx].pid[pid_idx];
+        }
+        adaptConfig.configs[conf_idx].bias = msg.configs[conf_idx].bias;
+        adaptConfig.thresholds[conf_idx] = msg.thresholds[conf_idx];
+      }
+      
+
+      size_t buf_size = jeroboam_datatypes_actuators_motion_AdaptativePIDConfig_0_1_SERIALIZATION_BUFFER_SIZE_BYTES_;
+      uint8_t buffer[jeroboam_datatypes_actuators_motion_AdaptativePIDConfig_0_1_SERIALIZATION_BUFFER_SIZE_BYTES_];
+
+      jeroboam_datatypes_actuators_motion_AdaptativePIDConfig_0_1_serialize_(&adaptConfig, buffer, &buf_size);
+
+      send_can_msg(MOTION_SET_ADAPTATIVE_PID_ID, &transfer_id, buffer, buf_size);
+    }
+
+    void motionConfigCB( const jrb_msgs::msg::MotionConfig msg) const {
+      static CanardTransferID transfer_id = 0;
+
+      jeroboam_datatypes_actuators_motion_MotionConfig_0_1 motionConfig;
+      
+      motionConfig.wheel_base.meter = msg.wheel_base;
+      motionConfig.left_wheel_radius.meter = msg.left_wheel_radius;
+      motionConfig.right_wheel_radius.meter = msg.right_wheel_radius;
+      motionConfig.maxLinearSpeed.meter_per_second = msg.max_linear_speed;
+      motionConfig.maxAngularSpeed.radian_per_second = msg.max_angular_speed;
+      motionConfig.maxLinearAccl.meter_per_second_per_second = msg.max_linear_accl;
+      motionConfig.maxAngularAccl.radian_per_second_per_second = msg.max_angular_accl;
+
+
+      size_t buf_size = jeroboam_datatypes_actuators_motion_MotionConfig_0_1_SERIALIZATION_BUFFER_SIZE_BYTES_;
+      uint8_t buffer[jeroboam_datatypes_actuators_motion_MotionConfig_0_1_SERIALIZATION_BUFFER_SIZE_BYTES_];
+
+      jeroboam_datatypes_actuators_motion_MotionConfig_0_1_serialize_(&motionConfig, buffer, &buf_size);
+
+      send_can_msg(MOTION_SET_MOTION_CONFIG_ID, &transfer_id, buffer, buf_size);
+    }
+
 
     rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr       odom_pub;
     rclcpp::Publisher<jrb_msgs::msg::PIDState>::SharedPtr       left_pid_pub;
@@ -312,11 +351,17 @@ class CanBridge : public rclcpp::Node
     rclcpp::Publisher<jrb_msgs::msg::PumpStatus>::SharedPtr     right_pump_pub;
     rclcpp::Publisher<jrb_msgs::msg::ValveStatus>::SharedPtr    left_valve_pub;
     rclcpp::Publisher<jrb_msgs::msg::ValveStatus>::SharedPtr    right_valve_pub;
+
+  
     rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr  twist_sub;
     rclcpp::Subscription<jrb_msgs::msg::PumpStatus>::SharedPtr  left_pump_sub;
     rclcpp::Subscription<jrb_msgs::msg::PumpStatus>::SharedPtr  right_pump_sub;
     rclcpp::Subscription<jrb_msgs::msg::ValveStatus>::SharedPtr left_valve_sub;
     rclcpp::Subscription<jrb_msgs::msg::ValveStatus>::SharedPtr right_valve_sub;
+    rclcpp::Subscription<jrb_msgs::msg::AdaptativePIDConfig>::SharedPtr left_adpat_pid_conf_sub;
+    rclcpp::Subscription<jrb_msgs::msg::AdaptativePIDConfig>::SharedPtr right_adpat_pid_conf_sub;
+    rclcpp::Subscription<jrb_msgs::msg::MotionConfig>::SharedPtr motion_config_sub;
+
 
 };
 
@@ -482,7 +527,7 @@ void createSubscriptions(void) {
             jeroboam_datatypes_actuators_pneumatics_PumpStatus_0_1_EXTENT_BYTES_);
   subscribe(CanardTransferKindMessage,
             ACTION_VALVE_STATUS_ID,
-            jeroboam_datatypes_actuators_pneumatics_ValveStatus_0_1_EXTENT_BYTES_);
+            jeroboam_datatypes_actuators_pneumatics_ValveStatus_0_1_EXTENT_BYTES_); 
 }
 
 void publishReceivedMessage(CanardRxTransfer * transfer) {
